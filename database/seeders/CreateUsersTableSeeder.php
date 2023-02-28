ac4f35db1e18b22cda3ea9b92ec812491492bced
DelosReyes-Rolly <rollydelosreyes729@gmail.com>
2023-03-01 04:11:48 +0800

    migration

--- database/seeders/CreateUsersTableSeeder.php
+++ database/seeders/CreateUsersTableSeeder.php
@@ -19,12 +19,5 @@ class CreateUsersTableSeeder extends Seeder
             'is_admin'=>'1',
             'password'=> bcrypt('12345678'),
         ]);
-
-        User::create([
-            'name'=>'User',
-            'email'=>'user@gmail.com',
-            'is_admin'=>'0',
-            'password'=> bcrypt('12345678'),
-        ]);
     }
 }
