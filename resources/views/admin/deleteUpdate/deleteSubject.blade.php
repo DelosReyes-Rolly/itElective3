2457adbfca180448f7d7bfc5b25409b80b883ac0
DelosReyes-Rolly <rollydelosreyes729@gmail.com>
2023-03-01 02:55:54 +0800

    update on delete

--- resources/views/admin/deleteUpdate/deleteSubject.blade.php
+++ resources/views/admin/deleteUpdate/deleteSubject.blade.php
@@ -1,5 +1,5 @@
 <div class="modal-header">
-    <h1 class="modal-title" id="staticBackdropLabel" style="font-size: 20px;">Delete Strand </h1>
+    <h1 class="modal-title" id="staticBackdropLabel" style="font-size: 20px;">Delete Subject </h1>
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
         <span aria-hidden="true">&times;</span>
     </button>
